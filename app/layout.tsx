6c25c785b76f8a3d83d9cc96372dbe75373cf34a
Aruna Joy <arunajoy2602@gmail.com>
2026-07-06 16:14:10 +0100

    bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,13 @@ export default function RootLayout({
       lang="en"
       className={`${plusJakartaSans.variable} ${inter.variable} h-full antialiased`}
     >
+      <head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
